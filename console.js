17e4b7e1533d7caec0488ace51d283d4adf8e4b0
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-11-01 18:03:32 +0900

    Update console.js

--- console.js
+++ console.js
@@ -1,38 +1,45 @@
 var devtool=document.createElement("code");
-devtool.style="background-color:#f0f1f2;display:block;height:100%;width:30%;position:absolute;z-index:9999999;top:0px;right:0px;position:fixed;border-left:solid 2px #c7c8c9;overflow:scroll;"
+devtool.style="background-color:#f0f1f2;display:block;height:calc(100% - 40px);width:30%;position:absolute;z-index:9999999;top:0px;right:0px;position:fixed;border-left:solid 2px #c7c8c9;overflow:scroll;word-wrap: break-word;color:#000000;";
 devtool.id="console";
-devtool.innerHTML=`<div style="font-weight:600;padding:3px;border-bottom:solid 2px #000000;margin-bottom:20px;">コンソール(開発者ツールが使えない人向け。エラーは表示できません。)</div>`
+devtool.innerHTML=`<div style="font-weight:600;padding:3px;border-bottom:solid 2px #000000;margin-bottom:5px;">コンソール(開発者ツールが使えない人向け。エラーは表示できません。)</div>`;
 var textbox=document.createElement("textarea");
 textbox.style="border:none;outline: none;position:absolute;bottom:0px;right:0px;width:29.2%;padding:2px;position:fixed";
 textbox.placeholder=">_";
 textbox.id="_console_textbox";
 devtool.appendChild(textbox);
+var submit=document.createElement("button");
+submit.innerHTML="Run!";
+submit.id="_Run_button";
+submit.style="background-color:#000000;color:#ffffff;padding;10px;border-radius:5px;corsor:pointer;font-size:5px;margin-left:5px;margin-bottom:10px;"
+devtool.lastChild.before(submit);
 document.body.firstChild.before(devtool);
 (function(){
   const log = console.log;
   console.log = function(...args){
     log(...args);
     const li = document.createElement('div');
-    li.style="border-bottom:solid 1px #c7c8c9"
-    li.innerText = args[0];
+    li.style="border-bottom:solid 1px #c7c8c9";
+    li.innerText = "> "+args[0];
     document.getElementById('console').appendChild(li);
   }
-})()
-document.getElementById("_console_textbox").addEventListener('keypress', test_ivent);
+})();
+document.getElementById("_Run_button").addEventListener('click', test_ivent);
 function test_ivent(e) {
-if(e.keyCode === 13){
-		console.log(document.getElementById("_console_textbox").value);
+    const li = document.createElement('div');
+    li.style="border-bottom:solid 1px #c7c8c9;padding:3px;opacity:0.7;";
+    li.innerText = document.getElementById("_console_textbox").value;
+    document.getElementById('console').appendChild(li);
     var script=document.createElement("script");
     script.id="consolescript";
-    script.innerHTML=document.getElementById("_console_textbox").value;
+    var code=document.getElementById("_console_textbox").value;
+    script.innerText =code ;
+    script.textContent = code;
+    document.body.appendChild(script);
     document.getElementById("_console_textbox").remove();
     var textbox=document.createElement("textarea");
     textbox.style="border:none;outline: none;position:absolute;bottom:0px;right:0px;width:29.2%;padding:2px;position:fixed";
     textbox.placeholder=">_";
     textbox.id="_console_textbox";
     document.getElementById("console").appendChild(textbox);
-    
     textbox.focus();
-    document.getElementById("_console_textbox").addEventListener('keypress', test_ivent);
-	}
 }
