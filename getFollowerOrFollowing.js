6dd74fa25bcca7451ab2b774e06bff47b239e169
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-11-05 09:42:19 +0900

    Update getFollowerOrFollowing.js

--- getFollowerOrFollowing.js
+++ getFollowerOrFollowing.js
@@ -1,21 +1,31 @@
 async function func(){
     const followerCount=document.getElementsByClassName("box-head")[0].innerText.replace(/[^0-9]/g, '');
     const username=document.getElementsByClassName("box-head")[0].getElementsByTagName("a")[1].innerText;
-    let userList=[];
+    let userList="";
     console.log(followerCount);
     console.log(username);
+    document.getElementsByClassName("media-grid")[0].innerHTML="";
     for(let page=0;page<followerCount;page+=60){
-        var res = await (await fetch(`${location.href}?page=${Math.ceil(page/60)}`)).text();
-        console.log(1)
+        var res = await (await fetch(`${location.href}?page=${Math.ceil(page/60)+1}`)).text();
         var box=document.createElement("div");
         box.innerHTML=res;
         document.body.appendChild(box);
         var box2=document.getElementsByClassName("user thumb item");
-        for(let i=60;i<box2.length;i++){
-            userList.push(box2[i].innerHTML.split("/")[2]);
+        for(let i=0;i<box2.length;i++){
+            userList+=box2[i].innerHTML.split("/")[2]+"\n";
         }
-        box2.remove();
-        console.log(page)
+        box.remove();
+        console.log(`Loading... (${page}/${followerCount})`)
     }
+    console.log(`Loading... (${followerCount}/${followerCount})`)
+    const link=new Blob([userList],{type:"text/plain"});
+    const url = URL.createObjectURL(link);
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.download = username+"'s.txt";
+    a.href = url;
+    a.click();
+    a.remove();
+    URL.revokeObjectURL(url);
 }
 func();
