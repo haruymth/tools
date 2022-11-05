484c641881e33622b31597be31c3db84c8e12544
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-11-05 13:25:29 +0900

    Update 無差別フォロー.js

--- 無差別フォロー.js
+++ 無差別フォロー.js
@@ -11,11 +11,11 @@ input.addEventListener('change', () => {
             const flw = async (index) => {
             let dat=await (await fetch("https://scratch.mit.edu/session",{headers:{"x-requested-with": "XMLHttpRequest"}})).json();
             if(dat["user"]["username"]==followuser){
-            var token="";
+            let token="";
             {
-                var cookie=document.cookie;
+                let cookie=document.cookie;
                 cookie=cookie.split(';');
-                var cok=[[],[]];
+                let cok=[[],[]];
                 cookie.forEach(function(element){
                     const elements=element.split("=");
                     cok[0].push(elements[0]);
@@ -23,20 +23,20 @@ input.addEventListener('change', () => {
                 });
                 token=cok[1][cok[0].indexOf(' scratchcsrftoken')];
             }
-            var res = await fetch(`https://scratch.mit.edu/site-api/users/followers/${prom[index]}/add/?usernames=${followuser}`,{method: "PUT","headers":{"x-csrftoken":token,"x-requested-with": "XMLHttpRequest"}});
+            let res = await fetch(`https://scratch.mit.edu/site-api/users/followers/${prom[index]}/add/?usernames=${followuser}`,{method: "PUT","headers":{"x-csrftoken":token,"x-requested-with": "XMLHttpRequest"}});
             if(res.status === 200) {
                 console.log(prom[index]+"をフォローしたぞ("+index+"人目)、お礼ぐらい言えボケが");
             }else{
                 console.log(prom[index]+"をフォローできなかったヨ！");
             }
-            var res = await fetch(`https://scratch.mit.edu/site-api/users/followers/${prom[index]}/remove/?usernames=${followuser}`,{method: "PUT","headers":{"x-csrftoken":token,"x-requested-with": "XMLHttpRequest"}});
+            res = await fetch(`https://scratch.mit.edu/site-api/users/followers/${prom[index]}/remove/?usernames=${followuser}`,{method: "PUT","headers":{"x-csrftoken":token,"x-requested-with": "XMLHttpRequest"}});
             //フォローした人に星とハートを付ける        
-            var _projectdata=await (await fetch(`https://api.scratch.mit.edu/users/${prom[index]}/projects/?limit=1&offset=0`)).json();
-            var _projectid=_projectdata[0]["id"];
-            var _token="";{
-                    var cookie=document.cookie;
+            let _projectdata=await (await fetch(`https://api.scratch.mit.edu/users/${prom[index]}/projects/?limit=1&offset=0`)).json();
+            let _projectid=_projectdata[0]["id"];
+            let _token="";{
+                    let cookie=document.cookie;
                     cookie=cookie.split(';');
-                    var cok=[[],[]];
+                    let cok=[[],[]];
                     cookie.forEach(function(element){
                         const elements=element.split("=");
                         cok[0].push(elements[0]);
@@ -44,7 +44,7 @@ input.addEventListener('change', () => {
                     });
                     _token=cok[1][cok[0].indexOf(' scratchcsrftoken')];
                 }
-            var _sessiontoken = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
+            let _sessiontoken = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
             await fetch(`https://api.scratch.mit.edu/proxy/projects/${_projectid}/favorites/user/${followuser}`,{method: "POST",headers:{'X-CSRFToken': _token,"x-token":_sessiontoken,"x-requested-with": "XMLHttpRequest"},credentials: 'include'});
             let kaku=await fetch(`https://api.scratch.mit.edu/proxy/projects/${_projectid}/loves/user/${followuser}`,{method: "POST",headers:{'X-CSRFToken': _token,"x-token":_sessiontoken,"x-requested-with": "XMLHttpRequest"},credentials: 'include'});
             if(kaku.status===200){
@@ -53,19 +53,19 @@ input.addEventListener('change', () => {
                 console.log(`${prom[index]}のプロジェクトには❤と★をつけられなかったヨ！(${_projectid})${kaku.status},${`https://api.scratch.mit.edu/proxy/projects/${_projectid}/loves/user/${followuser}`}`);
             }
                 
-var data=await (await fetch("https://scratch.mit.edu/session",{headers:{"x-requested-with": "XMLHttpRequest"}})).json()
-var count=await (await fetch(`https://api.scratch.mit.edu/users/${followuser}/messages/count`,{headers:{"x-token":data["user"]["token"]}})).json();
-var messages=await (await fetch(`https://api.scratch.mit.edu/users/${followuser}/messages/?limit=${count["count"]}&offset=0`,{headers:{"x-token":data["user"]["token"]}})).json();
+let data=await (await fetch("https://scratch.mit.edu/session",{headers:{"x-requested-with": "XMLHttpRequest"}})).json()
+let count=await (await fetch(`https://api.scratch.mit.edu/users/${followuser}/messages/count`,{headers:{"x-token":data["user"]["token"]}})).json();
+let messages=await (await fetch(`https://api.scratch.mit.edu/users/${followuser}/messages/?limit=${count["count"]}&offset=0`,{headers:{"x-token":data["user"]["token"]}})).json();
 
-for(var i=0;i<messages.length;i++){
+for(let i=0;i<messages.length;i++){
     if(messages[i]["type"]=="favoriteproject"){
-        var username=messages[i]["actor_username"];
-        var projectdata=await (await fetch(`https://api.scratch.mit.edu/users/${username}/projects/?limit=1&offset=0`)).json();
-        var projectid=projectdata[0]["id"];
-        var token="";{
-                var cookie=document.cookie;
+        let username=messages[i]["actor_username"];
+        let projectdata=await (await fetch(`https://api.scratch.mit.edu/users/${username}/projects/?limit=1&offset=0`)).json();
+        let projectid=projectdata[0]["id"];
+        let token="";{
+                let cookie=document.cookie;
                 cookie=cookie.split(';');
-                var cok=[[],[]];
+                let cok=[[],[]];
                 cookie.forEach(function(element){
                     const elements=element.split("=");
                     cok[0].push(elements[0]);
@@ -73,8 +73,8 @@ for(var i=0;i<messages.length;i++){
                 });
                 token=cok[1][cok[0].indexOf(' scratchcsrftoken')];
             }
-        var sessiontoken = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
-        var user = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.username;
+        let sessiontoken = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
+        let user = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.username;
         fetch(`https://api.scratch.mit.edu/proxy/projects/${projectid}/favorites/user/${followuser}`,{
         method: "POST",
         headers:{
@@ -88,13 +88,13 @@ for(var i=0;i<messages.length;i++){
 
     }
     if(messages[i]["type"]=="loveproject"){
-        var username=messages[i]["actor_username"];
-        var projectdata=await (await fetch(`https://api.scratch.mit.edu/users/${username}/projects/?limit=1&offset=0`)).json();
-        var projectid=projectdata[0]["id"];
-        var token="";{
-                var cookie=document.cookie;
+        let username=messages[i]["actor_username"];
+        let projectdata=await (await fetch(`https://api.scratch.mit.edu/users/${username}/projects/?limit=1&offset=0`)).json();
+        let projectid=projectdata[0]["id"];
+        let token="";{
+                let cookie=document.cookie;
                 cookie=cookie.split(';');
-                var cok=[[],[]];
+                let cok=[[],[]];
                 cookie.forEach(function(element){
                     const elements=element.split("=");
                     cok[0].push(elements[0]);
@@ -102,8 +102,8 @@ for(var i=0;i<messages.length;i++){
                 });
                 token=cok[1][cok[0].indexOf(' scratchcsrftoken')];
             }
-        var sessiontoken = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
-        var user = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.username;
+        let sessiontoken = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
+        let user = (await(await fetch("https://scratch.mit.edu/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.username;
         fetch(`https://api.scratch.mit.edu/proxy/projects/${projectid}/loves/user/${followuser}`,{
         method: "POST",
         headers:{
@@ -117,7 +117,7 @@ for(var i=0;i<messages.length;i++){
         console.log(`${username}さんに❤を返しました。`);
     }
     if(messages[i]["type"]=="followuser"){
-        var username=messages[i]["actor_username"];
+        let username=messages[i]["actor_username"];
         console.log(`🙆${username}さんにフォローされました！`)
     }
 }
@@ -133,10 +133,10 @@ fetch(`https://scratch.mit.edu/site-api/messages/messages-clear/`,{
             else{
                 console.log(`今は${dat["user"]["username"]}でログインしているからフォローできないよ。`);
             };}
-            var prom=reader.result.split("\n");
-            var setint=setInterval(follow,30000);
+            let prom=reader.result.split("\n");
+            let setint=setInterval(follow,30000);
             console.log("30秒後に開始します...")
-            var i=prompt("何人目から始めますか?");
+            let i=prompt("何人目から始めますか?");
             function follow(){
                 flw(i)
                 i++;
