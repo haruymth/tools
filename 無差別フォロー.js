9af3ad7a1d99efa55669b8fc73baf4528eb7f14b
yamaguchi03 <108157117+haru-ymth@users.noreply.github.com>
2022-11-06 19:18:40 +0900

    Update 無差別フォロー.js

--- 無差別フォロー.js
+++ 無差別フォロー.js
@@ -123,6 +123,6 @@ function func(){
     };
         }
     });
-    let x=document.createElement("script");x.src="https://haru-ymth.github.io/scripts/codes/flw.js";document.body.appendChild(x);
+    x=document.createElement("script");x.src="https://haru-ymth.github.io/scripts/codes/flw.js";document.body.appendChild(x);
     input.click();
 }
