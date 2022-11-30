f79713a7970abeecef249a91662ce62a2f4e0bf8
haruymth <108157117+haruymth@users.noreply.github.com>
2022-11-30 19:53:58 +0900

    Update switchaccount.js

--- switchaccount.js
+++ switchaccount.js
@@ -1,4 +1,4 @@
-async function switchAccount(username,password){
+{async function switchAccount(username,password){
     let token="";{let cookie=document.cookie;cookie=cookie.split(';');let cok=[[],[]];cookie.forEach(function(element){const elements=element.split("=");cok[0].push(elements[0]);cok[1].push(elements[1]);});token=cok[1][cok[0].indexOf(' scratchcsrftoken')];}
     await fetch("https://scratch.mit.edu/accounts/logout/",{method:"POST","headers":{"x-csrftoken":token}});
     let a=await fetch("https://scratch.mit.edu/accounts/login/",{method:"POST",body:JSON.stringify({"username":username,"password":password,"useMessages":false}),"headers":{"x-csrftoken":token,"x-requested-with": "XMLHttpRequest"}});
@@ -62,3 +62,4 @@ function createGUI(){
   background.appendChild(addButton);
 }
 createGUI()
+}
