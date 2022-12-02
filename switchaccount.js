2c6d83461dda620f87f57f984e117184866b6340
haruymth <108157117+haruymth@users.noreply.github.com>
2022-12-02 18:05:11 +0900

    Update switchaccount.js

--- switchaccount.js
+++ switchaccount.js
@@ -44,16 +44,27 @@
 		document.getElementById("switch_background").remove();
 		createGUI();
 	}
+	async function getMessagesCount(){
+		let elementList=document.getElementsByClassName("switch_oneaccount");
+		for(let i=0;i<elementList.length;i++){
+			let urname=elementList[i].textContent;
+			let count=await (await fetch("//api.scratch.mit.edu/users/"+urname+"/messages/count")).json()
+			let messagesBatch=elementList[i].nextElementSibling;
+			messagesBatch.innerHTML=count.count;
+		}
+	}
+	const messages=setInterval(getMessagesCount,5000);
 	function createGUI(){
 		let background=document.createElement("div");
-    background.style="background-color:#2a2a2a;height:400px;width:300px;position:absolute;top:10px;left:10px;position:fixed;border-radius:5px;color:#ffffff;font-size:15px;overflow:auto;z-index:2147483646";
-    background.id="switch_background";
+    	background.style="background-color:#2a2a2a;height:400px;width:300px;position:absolute;top:10px;left:10px;position:fixed;border-radius:5px;color:#ffffff;font-size:15px;overflow:auto;z-index:2147483646";
+    	background.id="switch_background";
 		document.body.appendChild(background);
 		let closeButton=document.createElement("span");
 		closeButton.innerHTML="x";
 		closeButton.style="color:#ffffff;display:inline-block;position:absolute;top:10px;left:306px;user-select:none;padding:10px;padding-top:5px;padding-bottom:5px;cursor:pointer;position:fixed;z-index:2147483647;background-color:#2a2a2a;border-radius:20%;";
 		closeButton.onclick=function(){
 			document.getElementById("switch_background").remove()
+			clearInterval(messages);
 		}
 		background.appendChild(closeButton);
 		let list=accountList();
@@ -61,8 +72,8 @@
 			let oneAccount=document.createElement("div");
 			oneAccount.innerHTML=list.accountList[i].username;
 			oneAccount.classList.add("switch_oneaccount");
-			oneAccount.dataset.password=list.accountList[i].password;
 			oneAccount.style="display:inline-block;padding:5px;margin-left:10px;border-bottom:solid 1px #424242;cursor:pointer;width:80%;vertical-align:middle;";
+			oneAccount.dataset.password=list.accountList[i].password;
 			oneAccount.onclick=async function(){
 				await switchAccount(this.textContent,this.dataset.password);location.reload()
 			};
@@ -85,6 +96,9 @@
 			icon.src="https://uploads.scratch.mit.edu/get_image/user/"+list.accountList[i].icon+"_32x32.png";
 			icon.style="vertical-align:middle;margin:2px;"
 			oneAccount.insertBefore(icon,oneAccount.firstChild);
+			let messagesBatch=document.createElement("span");
+			messagesBatch.style="display:inline-block;border-radius:50%;color:#ffffff;background-color:#ffb300;height:18px;width:18px;position:absolute;right:70px;text-align:center;vertical-align:baseline;margin-top:10px;";
+			oneAccount.after(messagesBatch);
 	  }
 		let addButton=document.createElement("div");
 		addButton.style="display:block;width:90%;margin:5%;margin-right:5%;margin-left:5px;text-align:center;cursor:pointer;font-weight:550;";
@@ -97,4 +111,5 @@
 		background.appendChild(addButton);
 	}
 	createGUI()
+	getMessagesCount();
 }
