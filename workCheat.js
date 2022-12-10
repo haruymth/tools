799fe93e12d192ea044a03dd9e8f6de145263b12
haruymth <108157117+haruymth@users.noreply.github.com>
2022-12-10 18:22:48 +0900

    Update workCheat.js

--- workCheat.js
+++ workCheat.js
@@ -14,7 +14,7 @@ async function cheat(){
       }
     let res=await (await fetch(`https://player.lifeistech-lesson.jp/api/lesson_player/scenario?scenario_path=${getA().scenario_path}&project_name=${getA().project_name}`)).json();
     //console.log(res)
-    let id=res.steps[res.steps.length-1].stepId;
+    let id=res.steps[res.steps.length-1].stepId-1;
     let newUrl=`https://player.lifeistech-lesson.jp/player/step?project_name=${getA().project_name}&scenario_path=${getA().scenario_path}&step_id=${id}`
     location.href=newUrl
 
