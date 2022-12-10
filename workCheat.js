4483d9e59973e3b604ac47a855737062dd2f9d14
haruymth <108157117+haruymth@users.noreply.github.com>
2022-12-11 08:14:46 +0900

    Update workCheat.js

--- workCheat.js
+++ workCheat.js
@@ -12,20 +12,41 @@ async function cheat(){
       
         return queries;
       }
-    let corces=await (await fetch(`https://api.lifeistech-lesson.jp/api/players/courses`).{}son();
+    function getCookieValue(key){
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+        var cookiesArray = cookie.split('='); 
+        if (cookiesArray[0].trim() == key.trim()) { 
+            return cookiesArray[1];  // (key[0],value[1])
+        }
+    }
+    return '';
+}
+    let corces=await (await fetch(`https://api.lifeistech-lesson.jp/api/players/courses`,{
+        headers:{
+            "authorization":"Bearer "+getCookieValue("mozermovie")
+        }
+    })).json();
     let lessonList=[];
-    for(let a=0;a<corces.corces.length;a++){
-        let corce1=corces.corces[a];
+    console.log(corces)
+    for(let a=0;a<corces.courses.length;a++){
+        let corce1=corces.courses[a].chapters;
+        console.log(corce1)
         for(let b=0;b<corce1.length;b++){
-            let corce2=corce1.chapter[b];
-            for(let c=0;c<corce2.length;c++){
+            let corce2=corce1[b];
+            console.log(corce2)
+            for(let c=0;c<corce2.lessons.length;c++){
                    let corce3=corce2.lessons[c];
-                   for(let d=0;d<corce3.length;d++){
-                       lessonList.push([corce2.id,corce3.id]);
-                   }
+                   console.log(corce3)
+                    lessonList.push([corce2.id,corce3.id]);
             }
         }
-        
+    }
+    for(let i=0;i<lessonList;i++){
+        await fetch("https://player.lifeistech-lesson.jp/api/lesson_player/lesson_finished",{
+        headers:{
+            "credentials":"include"
+        }})
     }
     console.log(lessonList)
     /*let res=await (await fetch(`https://player.lifeistech-lesson.jp/api/lesson_player/scenario?scenario_path=${getA().scenario_path}&project_name=${getA().project_name}`)).json();
