3e50696cee23ff2d3e66df44273688390dcaa9ff
haruymth <108157117+haruymth@users.noreply.github.com>
2022-12-10 18:11:33 +0900

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,23 +1,22 @@
 async function cheat(){
-    function getUrlQueries() {
-        var queryStr = window.location.search.slice(1);  // 文頭?を除外
+    function getA() {
+        var queryStr = window.location.search.slice(1);
             queries = {};
-      
-        // クエリがない場合は空のオブジェクトを返す
         if (!queryStr) {
           return queries;
         }
-      
-        // クエリ文字列を & で分割して処理
         queryStr.split('&').forEach(function(queryStr) {
-          // = で分割してkey,valueをオブジェクトに格納
           var queryArr = queryStr.split('=');
           queries[queryArr[0]] = queryArr[1];
         });
       
         return queries;
       }
-    let url;
-    let res=await (await fetch("https://player.lifeistech-lesson.jp/api/lesson_player/scenario?scenario_path=lesson%2Fpcbasic_chromebook&project_name=webdesign")).json();
+    let res=await (await fetch(`https://player.lifeistech-lesson.jp/api/lesson_player/scenario?scenario_path=${getA().scenario_path}&project_name=${getA().project_name}`)).json();
+    //console.log(res)
+    let id=res.steps[res.steps.length-1].stepId;
+    let newUrl=`https://player.lifeistech-lesson.jp/player/step?project_name=${getA().project_name}&scenario_path=${getA().scenario_path}&step_id=${id}`
+    location.href=newUrl
 
 }
+cheat()
